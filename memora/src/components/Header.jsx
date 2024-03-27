31abf37369e467cfd6ea2e54ba6586748c2cd12c
valerie smith <valerie.plante.11@gmail.com>
2024-03-27 13:58:45 -0400

    changed some image imports

--- memora/src/components/Header.jsx
+++ memora/src/components/Header.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "../styles/header.css"
-import MemoraLogo from '../../public/MemoraLogo.png'
+import MemoraLogo from '/MemoraLogo.png'
 
 export default function Header(){
     return(
